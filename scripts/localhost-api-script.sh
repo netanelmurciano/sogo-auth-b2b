b2c13ce26ae82698370135fe80e53ce3c4ed9235
Netanel <nmurciano1@gmail.com>
2019-02-24 09:37:52 +0200

    sogo-auth-project

---
+++ scripts/localhost-api-script.sh
@@ -0,0 +1,12 @@
+php artisan down
+composer clear-cache
+php artisan cache:clear
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+#php artisan key:generate
+#php artisan optimize
+composer dump-autoload
+
+# Start application server.
+php artisan up
